988ea61144369f40619d218946a8d62c0154f7e1
kogkoba <kc.koba@gmail.com>
2025-03-11 00:16:45 +0900

    Update tilemap_village.js

--- js/tilemap_village.js
+++ js/tilemap_village.js
@@ -26,19 +26,19 @@ const tileMapVillage = [
 
 /** タイル番号と画像パスの対応 */
 const tileImagesVillage = {
-  1: "assets/images/tiles/village/1grass.png",  // 草地
-  4: "assets/images/tiles/village/4hanabatake.png",  // 花畑
-  7: "assets/images/tiles/village/7bougu.png",  // 防具屋
-  9: "assets/images/tiles/village/9matinohei.png",  // 道のへり
-  10: "assets/images/tiles/village/10matinomiti.png", // 道
-  11: "assets/images/tiles/village/11yado.png", // 宿屋
-  12: "assets/images/tiles/village/12buki.png", // 武器屋
-  13: "assets/images/tiles/village/13kajiya.png", // 鍛冶屋
-  14: "assets/images/tiles/village/14yado.png", // 宿屋
-  15: "assets/images/tiles/village/15bed1.png", // ベッド1
-  16: "assets/images/tiles/village/16bed2.png", // ベッド2
-  17: "assets/images/tiles/village/17temple.png", // 神殿
-  18: "assets/images/tiles/village/18kusuri.png", // 薬屋
+  1: "./assets/images/tiles/village/1grass.png",  // 草地
+  4: "./assets/images/tiles/village/4hanabatake.png",  // 花畑
+  7: "./assets/images/tiles/village/7bougu.png",  // 防具屋
+  9: "./assets/images/tiles/village/9matinohei.png",  // 道のへり
+  10: "./assets/images/tiles/village/10matinomiti.png", // 道
+  11: "./assets/images/tiles/village/11yado.png", // 宿屋
+  12: "./assets/images/tiles/village/12buki.png", // 武器屋
+  13: "./assets/images/tiles/village/13kajiya.png", // 鍛冶屋
+  14: "./assets/images/tiles/village/14yado.png", // 宿屋
+  15: "./assets/images/tiles/village/15bed1.png", // ベッド1
+  16: "./assets/images/tiles/village/16bed2.png", // ベッド2
+  17: "./assets/images/tiles/village/17temple.png", // 神殿
+  18: "./assets/images/tiles/village/18kusuri.png", // 薬屋
 };
 
 
